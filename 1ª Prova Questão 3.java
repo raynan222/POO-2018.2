c646b2ba4daa2f0734020ace6e2ae950ecce1636
raynan222 <38507894+raynan222@users.noreply.github.com>
2018-09-06 16:21:40 -0300

    Create 1ª Prova Questão 3.java

---
+++ 1ª Prova Questão 3.java
@@ -0,0 +1,21 @@
+public class numeralis{
+	public static void main(String args[]){
+		int maior;
+		int menor;
+		float media;
+		for(int i=0;i<args.length;i++){
+			int n= Interfer.parseInt(args[i]);
+			media += n;
+			if(i==0){
+				maior=n;
+				menor=n;
+			}
+			else{
+				if(maior<n) maior=n;
+				if(menor>n) menor=n;
+			}
+			media = media/args.length;
+		}
+		syso(media menor maior);
+	}
+}
